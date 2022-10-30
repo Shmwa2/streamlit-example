876a03842941f2b5208a0d6465695f1799550d02
Shmwa2 <54296402+Shmwa2@users.noreply.github.com>
2022-10-30 11:04:01 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,6 +17,11 @@ In the meantime, below is an example of what you can do with just a few lines of
 
 st.title('My Parents New Healty Diner')
 
+st.header('Breakfast Menu')
+st.text('Omega 3 & Blueberry Oatmeal')
+st.text('Kale, Spinach & Rocket Smoothie')
+st.text('Hard-Boiled Free-Range Egg')
+
 
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
