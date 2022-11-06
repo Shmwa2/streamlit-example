71a3c160eeb73b9539d28a097dc4c1bd7c1ecfae
Shmwa2 <54296402+Shmwa2@users.noreply.github.com>
2022-11-06 23:07:06 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,4 +58,10 @@ with st.echo(code_location='below'):
     # 自分のコメントを書く - これは何をするの? 
     st.dataframe(fruityvice_normalized)
 
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_cur = my_cnx.cursor()
+    my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+    my_data_row = my_cur.fetchone()
+    st.text("Hello from Snowflake:")
+    st.text(my_data_row)
     
